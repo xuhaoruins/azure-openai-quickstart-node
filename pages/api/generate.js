99bc0ec7b36435f37c2b495e9b6e929505c01b95
xuhaoruins <xuhaoruins@hotmail.com>
2023-02-17 14:18:53 +0800

    for Azure

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,11 +1,14 @@
 import { Configuration, OpenAIApi } from "openai";
 
+
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  //apiKey: process.env.OPENAI_API_KEY,  
+  basePath: "https://aoai-lab.openai.azure.com/openai/deployments/text-davinci-003",
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
+  /*
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -14,12 +17,13 @@ export default async function (req, res) {
     });
     return;
   }
+*/
+  const chatMsg = req.body.chatMsg || '';
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  if (chatMsg.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid chatMsg",
       }
     });
     return;
@@ -28,9 +32,18 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
-    });
+      prompt: chatMsg,
+      temperature: 0.7,
+      max_tokens: 1000,
+
+    },{
+      headers: {
+        'api-key': process.env.OPENAI_API_KEY,
+      },
+      params: { "api-version": "2022-12-01" }
+    },
+    );
+
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
@@ -47,16 +60,17 @@ export default async function (req, res) {
     }
   }
 }
+/*
+function generatePrompt(chatMsg) {
+  const capitalizedchatMsg =
+    chatMsg[0].toUpperCase() + chatMsg.slice(1).toLowerCase();
+  return `Suggest three names for an chatMsg that is a superhero.
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
+chatMsg: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
+chatMsg: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
+chatMsg: ${capitalizedchatMsg}
 Names:`;
 }
+*/
